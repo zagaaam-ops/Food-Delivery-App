8afbc9abc42ed360ed067c4d83c13870fd3a534e
zagaaam-ops <zagaaam@gmail.com>
2026-08-02 12:17:29 +0000

    Fix: Update dependencies to match Flutter 3.32.x SDK requirements

--- pakistan-delivery-app/mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pakistan-delivery-app/mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,22 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import file_selector_macos
-import flutter_secure_storage_darwin
-import geocoding_darwin
+import flutter_secure_storage_macos
 import geolocator_apple
-import package_info_plus
 import shared_preferences_foundation
-import sqflite_darwin
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
-  GeocodingDarwinPlugin.register(with: registry.registrar(forPlugin: "GeocodingDarwinPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
